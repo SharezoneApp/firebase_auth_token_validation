9c800e2389faa5d9ddb6a65690de7c32a52c3aa9
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2020-11-02 23:29:54 +0100

    run_e2e_test.shconditional .env file

--- run_e2e_test.sh
+++ run_e2e_test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Must be ". script" so that vars are exported to this shell
-. ./load_env_into_bash.sh
+# On CI-Pipeline env-vars are pre-exported via Github secrets not via .env file.
+if [-f ".env"]; then
+    # Must be ". script" so that vars are exported to this shell
+    . ./load_env_into_bash.sh
+fi
 
 cd firebase_authentication_token_validator
 dart test e2e_test
