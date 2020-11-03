874f26fc5809541093a831997e2a6efe411adc06
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2020-11-03 21:00:12 +0100

    fix attempt
    "Warning: Pub installs executables into $HOME/.pub-cache/bin, which is not on your path.
    Activated tuneup 0.3.7.
    You can fix that by adding this to your shell's config file (.bashrc, .bash_profile, etc.):
    
      export PATH="$PATH":"$HOME/.pub-cache/bin"
    "

--- check_for_lint_errors_and_TODO_comments.sh
+++ check_for_lint_errors_and_TODO_comments.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+# To make pub global work
+export PATH="$PATH":"$HOME/.pub-cache/bin"
 pub global activate tuneup
 # TODOs should be Tickets - not TODO comments.
 tuneup check ./firebase_auth_token_validation --fail-on-todos
