46b078f279c8d3b87a6e1231549f531a44882f43
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2020-11-03 19:08:04 +0100

    Package-Name zu firebase_auth_token_validation geändert.

--- firebase_auth_token_validation/lib/firebase_auth_token_validation.dart
+++ firebase_auth_token_validation/lib/firebase_auth_token_validation.dart
@@ -1,8 +1,8 @@
 /// Support for doing something awesome.
 ///
 /// More dartdocs go here.
-library firebase_authentication_token_validator;
+library firebase_auth_token_validation;
 
-export 'src/firebase_authentication_token_validator_base.dart';
+export 'src/firebase_auth_token_validation_base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
