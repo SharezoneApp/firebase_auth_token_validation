d544a4e1372c6a91a139622cc4e632c9be4aee5d
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2020-11-04 20:05:21 +0100

    Fixed spacing in bash if condition.
    The spacing gave the error "[-f: command not found" which means that the .env would not be read.

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 # On CI-Pipeline env-vars are pre-exported via Github secrets not via .env file.
-if [-f ".env"]; then
+if [ -f ".env" ]; then
     # Must be ". script" so that vars are exported to this shell
     . ./load_env_into_bash.sh
 fi
