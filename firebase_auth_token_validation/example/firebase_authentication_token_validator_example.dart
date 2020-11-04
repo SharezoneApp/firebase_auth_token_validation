418da537e30fe926114c45ad78a8b5a30c956ed1
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2020-11-04 20:14:09 +0100

    Created a simple example. (#14)

--- firebase_auth_token_validation/example/firebase_authentication_token_validator_example.dart
+++ firebase_auth_token_validation/example/firebase_authentication_token_validator_example.dart
@@ -1,1 +1,16 @@
+import 'package:firebase_auth_token_validation/firebase_auth_token_validation.dart';
 
+// ignore_for_file:unused_local_variable
+
+Future<void> main() async {
+  final tokenValidator =
+      FirebaseAuthTokenValidator(firebaseProjectId: 'my-firebase-project');
+
+  // From e.g. a server request
+  final jwt = "your.jwt.here";
+  // Throws if JWT is invalid, returns JWT claims if sucessful.
+  final claims = await tokenValidator.validateJwt(jwt);
+  // Firebase adds the "user_id" claim which is the uid of the FirebaseAuth
+  // user.
+  final uid = claims['user_id'];
+}
